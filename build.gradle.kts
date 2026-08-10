62b8e9484779bba9c304f4cc4d78c1015bf084ce
KKKXia <1534119921@qq.com>
2026-08-10 14:55:19 +0800

    Update project configuration file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,10 @@
 
 plugins {
-    id("com.github.ElytraServers.elytra-conventions") version "v1.1.1"
+    id("com.github.ElytraServers.elytra-conventions") version "v1.1.2"
     id("com.gtnewhorizons.gtnhconvention")
+}
+
+minecraft {
+    extraRunJvmArguments.add("-Xmx8G")
+    extraRunJvmArguments.add("-Xms8G")
 }
